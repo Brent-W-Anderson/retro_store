e53250e85f060e5fa477558013ac44d7f8a3860e
gobrent21@gmail.com <gobrent21@gmail.com>
2022-03-22 00:05:32 -0500

    feat: center background & remove body element from return & make text contrast against background.

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -4,16 +4,16 @@ import { Component } from 'react';
 export default class HomePage extends Component {
   render() {
     return (
-      <body>
-      <div id='home' className="page">
-        <h2> Welcome to Retro Retro </h2>
-      </div>
+      <>
+        <div id='home' className="page">
+          <h2> Welcome to Retro Retro </h2>
+        </div>
 
-      <div id='rolodex'>
-        <h2>Featured Content</h2>
+        <div id='rolodex'>
+          <h2>Featured Content</h2>
 
-      </div>
-      </body>
+        </div>
+      </>
     );
   }
 }
