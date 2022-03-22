5d678dd3f09ebbfabbe5e8185279bb4a6e8be5b8
gobrent21@gmail.com <gobrent21@gmail.com>
2022-03-22 17:15:20 -0500

    feat: separate RAWG into component to be used on other pages.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,27 +6,27 @@ import Header from './header/header';
 import HomePage from './pages/home/home';
 import SearchPage from './pages/search/search';
 
+// API
+import RAWG from './RAWG/RAWG';
+
 // styling
 import './app.scss';
 
 export default class App extends Component<{ page:string }> {
   state = {
     games: [],
-    pageNum: 1
+    pageNum: 1 // change this to see a different page with more games or something else..
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     const { pageNum } = this.state;
+    const { page } = this.props;
 
-    const url = 'https://api.rawg.io/api'; // api url
-    const search = '/games?'; // what data to grab
-    const key = '&key=' + process.env.RAWG_API_KEY; // RAWG API KEY ( create and .env file at the root of the project with your own key )
-    const page = `&page=${ pageNum }`; // which page to target
-    const page_size = '&page_size=10'; // how many items to show on ea. page
-
-    await fetch( url + search + key + page + page_size )
-    .then( response => response.json() )
-    .then( data => this.setState({ games: data.results }) );
+    if( page === 'home' ) {
+      this.setState({
+        games: RAWG( 'games', pageNum ) // search API TODO: move this to a more specific location that we need it within the components.
+      })
+    }
   }
 
   render() {
