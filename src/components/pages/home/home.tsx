afde8b95bfd9a9ae8109364c5b6c329b1e5236c8
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2022-03-27 12:44:33 -0500

    Added homepage store menu
    added a menu for the store page, adding links as I go

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -26,8 +26,20 @@ export default class HomePage extends Component<{ offsetY:number, scrolling:bool
 
     return (
       <div id='home' className="page">
-        <div id='rolodex'>
-          <h3>Featured Content</h3>
+        <div id='homePageIntro'>
+          <h3>Welcome to Retro Retro</h3>
+        </div>
+        <div className='featured'>
+         This is where our rotating featured image should go
+         
+        </div>
+        <div className='click-container'>
+          <div className='box' ><a href='link'>On Sale</a></div>
+          <div className='box' >Featured Page</div>
+          <div className='box' >Xbox</div>
+          <div className='box' >Playstation</div>
+          <div className='box' >Nintendo</div>
+          <div className='box' >SEGA/NES</div>
         </div>
 
         <div className='games'>
