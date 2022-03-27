5dd00672a75cd86812645ab869ee7cc1c6977791
gobrent21@gmail.com <gobrent21@gmail.com>
2022-03-26 20:13:56 -0500

    feat: move RAWG API with scrollable parallax to search page and integrate with input value

--- src/components/RAWG/RAWG.tsx
+++ src/components/RAWG/RAWG.tsx
@@ -1,13 +1,14 @@
 
 export default async function RAWG( param:string, pageNum:number )  {
     const url = 'https://api.rawg.io/api'; // api url
-    const search = `/${ param }?`; // what data to grab
+    const search = `/games?`; // what data to grab
     const key = 'key=' + process.env.RAWG_API_KEY; // RAWG API KEY ( create and .env file at the root of the project with your own key )
+    const searchParam = `&search=${ param }`;
     const page = `&page=${ pageNum }`; // which page to target
     const page_size = '&page_size=20'; // how many items to show on ea. page
 
     async function fetchData() {
-        const data = await fetch( url + search + key + page + page_size )
+        const data = await fetch( url + search + key + searchParam + page + page_size )
         .then( response => response.json() )
         .then( data => {
             return data.results;
--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,46 +1,12 @@
 
 import { Component } from 'react';
 
-// components
-import GamesParallax from '../../gamesParallax/gamesParallax';
-
-// API
-import RAWG from '../../RAWG/RAWG';
-
-export default class HomePage extends Component<{ offsetY:number, scrolling:boolean }> {
-  state = {
-    games: []
-  }
-
-  async componentDidMount() {
-    const pageNum = Math.floor( Math.random() * 10 );
-
-    this.setState({
-      games: await RAWG( 'games', pageNum ) // fetch some games from our RAWG API
-    })
-  }
-
+export default class HomePage extends Component {
   render() {
-    const { games } = this.state;
-    const { offsetY, scrolling } = this.props;
-
     return (
       <div id='home' className="page">
         <div id='rolodex'>
           <h3>Featured Content</h3>
-        </div>
-
-        <div className='games'>
-          { games?.map( ( game: { id:number, background_image:string } ) => { 
-            return (
-              <GamesParallax 
-                key={ game.id } 
-                game={ game } 
-                offsetY={ offsetY }
-                scrolling={ scrolling }
-              />
-            );
-          } ) }
         </div>
       </div>
     );
