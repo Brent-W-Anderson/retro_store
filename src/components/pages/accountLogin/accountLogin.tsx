5662e84d4bf0bbf76154daabab98b696fd5c313e
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2022-03-27 12:43:29 -0500

    Acct page added
    Added account page

---
+++ src/components/pages/accountLogin/accountLogin.tsx
@@ -0,0 +1,13 @@
+
+import {Component} from 'react';
+
+
+export default class AccountLogin extends Component {
+    render() {
+        return (
+            <div id='accountLogin' className='page'>
+                <h3>Account Login</h3>
+            </div>
+        );
+    }
+}
