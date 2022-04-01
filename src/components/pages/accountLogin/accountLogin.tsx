fc5b582d24754af625262cb6f99254a92d32f59e
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2022-03-31 22:30:09 -0500

    Forms for Account Page
    feat: adding forms for existing user and new account

--- src/components/pages/accountLogin/accountLogin.tsx
+++ src/components/pages/accountLogin/accountLogin.tsx
@@ -1,13 +1,50 @@
 
 import {Component} from 'react';
-
+import { useState } from 'react';
+import { ReactDOM } from 'react';
 
 export default class AccountLogin extends Component {
-    render() {
+    
+    buttonAlerts() {
+        alert('Test');
+    }
+
+    function submitLogin() {
+        const[inputs,  setInputs] = useState({});
+
+        const handleChange = (event: { target: { userID: string; password: any; }; }) => {
+            const userID = event.target.userID;
+            const password = event.target.password;
+            setInputs(values => ({...values, [userID]: password}))
+        }
+        const handleSubmit = (event: { preventDefault: () => void; }) => {
+            event.preventDefault();
+            console.log(inputs);
+        }
+    
+
+    
         return (
-            <div id='accountLogin' className='page'>
-                <h3>Account Login</h3>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <label>Enter ID: 
+                    <input 
+                    type='text'
+                    name='userID'
+                    value={inputs.userID || ''}
+                    onChange={handleChange}
+                    />
+                </label>
+                <label>Password: 
+                    <input
+                    type='any'
+                    name='password'
+                    value={inputs.password || ''}
+                    onChange={handleChange}
+                    />
+                </label>
+            </form>
         );
+        ReactDOM.render(<submitLogin />, document.getElementById('root'));
     }
 }
+
