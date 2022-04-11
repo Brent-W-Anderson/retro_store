58de255e2d28a7430011a7d00d6b80b40a756f7c
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2022-04-10 20:29:47 -0500

    feat: username & pass feedback
    db connection works, ensure you upload db posted on trello to your mariaDB as 'retro'

--- src/components/pages/login/login.php
+++ src/components/pages/login/login.php
@@ -1,46 +1,45 @@
 
-<!-- TODO: look up documentation on PHP or watch a video on a basic setup for connecting your DB locally and just return a simple test: "hello world" from that table. -->
-
 <?php
   // Resources used
   // https://www.w3schools.com/php/php_mysql_connect.asp
 
   //must create db in mysql named 'retro' for this to connect succesfully
   if( $_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // login info
     $login = $_POST['login_username'];
     $password = $_POST['login_password'];
 
-    if (!$login || !$password) { // this is a little redundant to have because we are also going to check within the DB if a username or password exists
-      echo '<pre id="login_data"> You did not enter login information. </pre>';
+    // Database interaction
+
+    //creates connection to db
+    @$db = new mysqli('localhost', 'root', '', 'retro');
+
+    // This is in place to catch if the connection fails, if it triggers it means it didnt connect.
+    if ( $db->connect_error ) {
+      die("connection failed: " .$db->connect_error);
+    };
+      
+    // Statements for interaction with the DB
+    //Query the db for the username and passowrd entered
+    $userCk = $db -> query("SELECT username, password FROM customer WHERE username =  '$login' AND password = '$password'");
+
+
+    // if no result comes back from db, throws error stating why
+    if(!$userCk) { echo mysqli_error($db);}
+
+    if ($userCk->num_rows > 0) {
+      // output data of username/password that was matched in database
+      while($row = $userCk->fetch_assoc()) {
+        echo '<pre id="login_data"> Welcome to RETRO-RETRO: ' . $row["username"]. "<br> Password: " .$row["password"]. "<br>";
+
+      }
+    } else {
+      echo "0 results";
     }
-    else {
-      // Database stuff ( don't just say "Database stuff" ) -- this looks more like you're establishing a connection locally, be more specific
-      @$db = new mysqli('localhost', 'root', '', 'retro');
-      @$db = mysqli_connect('localhost,', 'root', '', 'retro');
-
-      // why did this connect_error fail? this doesn't provide you with much information if you're trying to see what the error was.
-      if ( mysqli_connect_errno() ) {
-        echo '<pre id="login_data" Database connection failed';
-        exit;
-      };
-
-      // leave yourself some notes here to describe what you're doing
-      $query = 'SELECT login FROM customer'; // why is a query wrapped in a string? Is this how it's supposed to be?
-      $stmt = $db -> prepare($query); // what does prepare do?
-      $stmt->execute();
-      $stmt->bind_result($login, $password);
-      $num_results = mysqli_num_rows($result);
-
-      // why would we need a while loop here?
-      while( $stmt->fetch() ) {
-        echo 'pre id="login_data"> Username: '.$login.' </pre>';
-      };
 
       // Closes the db
       $db->close();
 
-      // return data to frontend
-      echo '<pre id="login_data"> welcome to RETRO-RETRO: '.$login.' </pre>';
-    }
   }
 ?> 
--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -5,34 +5,47 @@ import { Component } from 'react';
 export default class Login extends Component { 
     state = {
         login_username: '',
-        login_password: ''
+        login_password: '',
+        username: '',
+        password: '',
+        email: '',
+        name: '',
+        phone: '',
+        address: '',
+        birthday: '',
+        account: []
     }
 
     iframeData = React.createRef<HTMLIFrameElement>();
 
+    // for login part
     checkData = () => {
         const loginData = this.iframeData.current?.contentWindow?.document.getElementById( 'login_data' );
     
         if( loginData?.textContent && loginData.textContent !== '' ) {
-        //   const txt = loginData.textContent; // convert to plain text to get rid of the crappy html that got carried over.
-        //   const txtString = String( txt ); // convert back to a normal string without the bs.
-        //   let txtJson;
+           const txt = loginData.textContent; // convert to plain text to get rid of the crappy html that got carried over.
+           const txtString = String( txt ); // convert back to a normal string without the bs.
+           let txtJson;
     
-        //   try {
-        //     txtJson = JSON.parse( txtString ); // break it into JSON.
-        //   }
-        //   catch( e ) {
-        //     console.error( 'error fetching data from DOM: ' + e );
+           try {
+             txtJson = JSON.parse( txtString ); // break it into JSON.
+           }
+           catch( e ) {
+             console.error( 'error fetching data from DOM: ' + e );
 
-        //     return true;
-        //   }
+             return true;
+           }
     
-        //   if( txtJson.results ) {
-        //     console.warn( txtJson ); // data being echo'd back (this will be in the iframe)
-      
-            console.warn( loginData.textContent ); // this is the data coming back from login.php
-            return true;
-        //   }
+           if( txtJson.results ) {
+
+                console.warn( loginData.textContent ); // this is the data coming back from login.php
+                this.setState({
+                    account: txtJson.results
+                });
+
+                console.warn( txtJson );  // data being echo'd back (this will be in the iframe)
+                return true;
+           }
         }
     
         return false;
@@ -60,16 +73,157 @@ export default class Login extends Component {
         });
     }
 
+
+    // for account creation part
+    handleSubmit = () => {
+        console.warn( 'sending the form to db... soon' );
+    }
+    handleNewUsername = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        this.setState({
+            username: e.target.value
+        });
+    }
+    handleNewPassword = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        this.setState({
+            password: e.target.value
+        });
+    }
+
+    handleEmail = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    handleName = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    handlePhone = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        this.setState({
+            phone: e.target.value
+        });
+    }
+
+    handleAddress = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        this.setState({
+            address: e.target.value
+        });
+    }
+
+    handleBirthday = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        this.setState({
+            birthday: e.target.value
+        });
+    }
+
     render() {
-        const { login_username, login_password } = this.state;
+        const { login_username, login_password, username, password, email, name, phone, address, birthday } = this.state;
 
         return (
             <div id='login' className='page'>
+            <form 
+            id='user_info' 
+            className='page'
+            method='POST'
+            action='./PHP/account.php'
+            target='creation_iframe'
+            onSubmit={this.handleSubmit}
+            >
+                <p>Create Account: </p>
+                <fieldset>
+                    <label> Enter Username: </label>
+                    <input 
+                        type='text'
+                        name='username'
+                        value={ username }
+                        onChange={ this.handleNewUsername }
+                        required
+                    />
+                </fieldset>
+                
+                <fieldset>
+                    <label> Password: </label>
+                    <input 
+                        type='text'
+                        name='login_password'
+                        value={ password }
+                        onChange={ this.handleNewPassword }
+                        required
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <label> Email: </label>
+                    <input 
+                        type='text'
+                        name='email'
+                        value={ email }
+                        onChange={ this.handleEmail }
+                        required
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <label> Full Name: </label>
+                    <input 
+                        type='text'
+                        name='fname'
+                        value={ name }
+                        onChange={ this.handleName }
+                        required
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <label> Phone: </label>
+                    <input 
+                        type='text'
+                        name='phone'
+                        value={ phone }
+                        onChange={ this.handlePhone }
+                        required
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <label> Address: </label>
+                    <input 
+                        type='text'
+                        name='address'
+                        value={ address }
+                        onChange={ this.handleAddress }
+                        required
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <label> Birthday: </label>
+                    <input 
+                        type='text'
+                        name='birthday'
+                        value={ birthday }
+                        onChange={ this.handleBirthday }
+                        required
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <button 
+                    type='submit'
+                    onClick={ this.handleSubmit }
+                    value='submit'></button>
+                </fieldset>
+            </form>
+
                 <form
                     method='POST'
                     action='./PHP/login.php'
                     target='login_iframe'
                 >
+                    <p>Enter your login information: </p>
                     <fieldset>
                         <label> Enter ID: </label>
                         <input id='username'
@@ -98,6 +252,16 @@ export default class Login extends Component {
                         style={{ display: 'none' }}
                     > Submit </button>
                 </form>
+
+                
+                <div className ='account_info'> 
+                    {
+                        //fill this with output formatting with data from php file
+
+
+                    }
+
+                </div>
 
                 <iframe ref={ this.iframeData } name='login_iframe' style={{ display: 'none' }} />
             </div>
