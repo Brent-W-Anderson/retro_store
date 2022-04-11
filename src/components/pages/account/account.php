74c95129f3ca897cd13309347f11f78e9e8ecee3
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2022-04-11 17:05:26 -0500

    feat: add in backend. corrected variable names
    set for future implementation of user insert if not in db.  it will validate and add user in the backend

--- src/components/pages/account/account.php
+++ src/components/pages/account/account.php
@@ -9,7 +9,7 @@
     $fullname = $_POST['fullname'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
-    $birthday = $_POST['birthday'];
+    $birthdate = $_POST['birthdate'];
 
 
     // Database interaction
@@ -23,16 +23,34 @@
     };
       
     // Statements for interaction with the DB
+    //Check if user already exists via Username, email.
+    // Working to set up so the db does the work of validating the user
+    /*
+    $user_verify = $db -> query("IF NOT EXISTS ( BEGIN
+                                                  SELECT username, email FROM customer 
+                                                  WHERE username = '$username' 
+                                                  AND email = '$email'
+                                                  
+                                                  BEGIN
+                                                    INSERT INTO customer (username, password, email, fullname, phone, address, birthdate)
+                                                    VALUES ('$username', '$password', '$email', '$fullname', '$phone',
+                                                            '$address', '$birthdate')
+                                                  END
+                                                  END");
+      */
+
+
     //Insert Record into DB
-    $insert_user = "INSERT INTO customer (username,  password, email, fullname, phone, address, birthdate)
-                                    VALUES ('$username', '$password', '$email', '$fullname', '$phone',
-                                            '$address', '$birthday')";
+    $insert_user = "INSERT INTO customer (username, password, email, fullname, phone, address, birthdate)
+    VALUES ('$username', '$password', '$email', '$fullname', '$phone',
+            '$address', '$birthdate')";
+    // Inserts the user into the db, if success prints success
     if( $db -> query($insert_user) === TRUE) {
         echo '<pre id="login_data"> User succesfully added ';
     } else {
         echo 'Error: ' .$sql. '<br>' .mysqli_error($db);
     }
-
+    
 
       // Closes the db
       $db->close();
