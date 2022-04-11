a5a880ddd989dc14465d33a965a07baaea2badf6
gobrent21@gmail.com <gobrent21@gmail.com>
2022-04-11 07:23:52 -0500

    notes

--- src/components/pages/account/account.tsx
+++ src/components/pages/account/account.tsx
@@ -2,17 +2,8 @@
 import { Component } from 'react';
 
 export default class Account extends Component {
-    state = {
-        username: '',
-        password: '',
-        email: '',
-        name: '',
-        phone: '',
-        address: '',
-        birthday: ''
-
-    }
-
+    // you probably won't need state here because the data will get passed in from the app component.
+    
     render() {
         return (
             <h3> ..show account specific information here (after a user has logged in).. </h3>
