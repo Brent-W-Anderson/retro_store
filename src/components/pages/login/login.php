75f08994b59d759d27c6d90bf2622bbc9cdfad52
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2022-04-21 22:28:45 -0500

    Update login.php
    the login page has been overhauled to check if user exists and display an error to the webpage if they dont. if they do exist it will print there account info and game_id they own to the webpage

--- src/components/pages/login/login.php
+++ src/components/pages/login/login.php
@@ -6,12 +6,11 @@
   //must create db in mysql named 'retro' for this to connect succesfully
   if( $_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // login info
+    // login info from POST
     $login = $_POST['login_username'];
     $password = $_POST['login_password'];
 
-    // Database interaction
-
+    // Database interactions
     //creates connection to db
     @$db = new mysqli('localhost', 'root', '', 'retro');
 
@@ -19,23 +18,42 @@
     if ( $db->connect_error ) {
       die("connection failed: " .$db->connect_error);
     };
-      
-    // Statements for interaction with the DB
-    //Query the db for the username and passowrd entered
+
+
+    // Validates if the user is in the database, prints account info if they are, prints error if they are not or incorrectly entered something
     $userCk = $db -> query("SELECT username, password FROM customer WHERE username =  '$login' AND password = '$password'");
 
+    if(!empty($userCk) && $userCk->num_rows == 1) {
 
-    // if no result comes back from db, throws error stating why
-    if(!$userCk) { echo mysqli_error($db);}
+      // Selecting the account info of the user who entered their login credentials
+      $account_info = $db -> query("SELECT username, email, fullname, phone, address, birthdate FROM customer WHERE username = '$login'");
 
-    if ($userCk->num_rows > 0) {
-      // output data of username/password that was matched in database
-      while($row = $userCk->fetch_assoc()) {
-        echo '<pre id="login_data"> Welcome to RETRO-RETRO: ' . $row["username"]. "<br> Password: " .$row["password"]. "<br>";
+      // Displays the data
+      if(!empty($account_info) && $account_info->num_rows > 0) {
+        while($user_row = $account_info->fetch_assoc()) {
+      // displays header and user info
+      echo "<h1> Welcome to your Account </h1>";
+      echo '<pre id="login_data"> Welcome to RETRO-RETRO: ' . $user_row["username"]. "<br> Address: " .$user_row["address"]. "<br> Phone: " .$user_row["phone"]. "<br>";
+      
+        }
 
+      //displays games that user owns
+      // XXXXXXXXX displays game ID right now, need to make display game and image of the games
+      $user_games = $db -> query("SELECT game_id FROM transaction WHERE username = '$login'");
+      if(!empty($user_games) && $user_games->num_rows > 0) {
+        echo "<br>Games Owned: <br>";
+        while($row = $user_games->fetch_assoc()) {
+          echo "Game name: " .$row['game_id']. "<br>";
+        }
       }
-    } else {
-      echo "0 results";
+      }
+      else {
+        echo "You dont own any games";
+      }
+    
+    }
+    else {
+      print "<h1> Login info incorrect, try again. </h1>";
     }
 
       // Closes the db
